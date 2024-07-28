300a3c4611f7f75260c2371b3f843d73de0307a4
tohara220 <tohara.220@gmail.com>
2024-07-28 11:53:29 +0900

    DeleteViewの追加

--- apps/todo/urls.py
+++ apps/todo/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
 from django.views.generic import RedirectView
 
-from apps.todo.views import TodoCreateView, TodoDetailView, TodoListView, TodoUpdateView
+from apps.todo.views import (
+    TodoCreateView,
+    TodoDeleteView,
+    TodoDetailView,
+    TodoListView,
+    TodoUpdateView,
+)
 
 urlpatterns = [
     path("", RedirectView.as_view(pattern_name="todo_list", permanent=True)),
@@ -9,4 +15,5 @@ urlpatterns = [
     path("todos/create/", TodoCreateView.as_view(), name="todo_create"),
     path("todos/<uuid:pk>/", TodoDetailView.as_view(), name="todo_detail"),
     path("todos/<uuid:pk>/update/", TodoUpdateView.as_view(), name="todo_update"),
+    path("todos/<uuid:pk>/delete/", TodoDeleteView.as_view(), name="todo_delete"),
 ]
--- apps/todo/views.py
+++ apps/todo/views.py
@@ -1,6 +1,6 @@
 from django.urls import reverse_lazy
 from django.views.generic import DetailView, ListView
-from django.views.generic.edit import CreateView, UpdateView
+from django.views.generic.edit import CreateView, DeleteView, UpdateView
 
 from apps.todo.forms import TodoForm
 from apps.todo.models import Todo
@@ -29,4 +29,10 @@ class TodoUpdateView(UpdateView):
     model = Todo
     form_class = TodoForm
     template_name = "todo_update.html"
+    success_url = reverse_lazy("todo_list")
+
+
+class TodoDeleteView(DeleteView):
+    model = Todo
+    template_name = "todo_delete.html"
     success_url = reverse_lazy("todo_list")
